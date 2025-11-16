ce6602ec6c780fe0c1d6d79bcc2156633f044a2c
Jander Mark <jmr.jr@hotmail.com>
2025-11-16 20:59:44 -0300

    URL da API para a vercel

--- task_manager_frontend/src/services/api.ts
+++ task_manager_frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { auth } from '@/firebaseConfig'; // Importe o auth
 
 // URL da sua API
-const API_URL = 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
